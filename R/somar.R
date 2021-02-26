195809654e13085806db2fdc20fe969a7b909094
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2021-02-26 12:01:30 -0300

    first commit

---
+++ R/somar.R
@@ -0,0 +1,14 @@
+#' Função para somar
+#'
+#' Esta função serve para somar dois números
+#'
+#' @param a Um número
+#' @param b Outro número
+#'
+#' @examples
+#' somar(3, 4)
+#'
+#' @export
+somar <- function(a, b){
+  a + b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,14 @@
+#' Função para subtrair
+#'
+#' Esta função serve para subtrair dois números
+#'
+#' @param a Um número
+#' @param b Outro número
+#'
+#' @examples
+#' subtrair(3, 4)
+#'
+#' @export
+subtrair <- function(a, b){
+  a - b
+}
